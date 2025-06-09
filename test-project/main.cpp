c2bc8a4d414b52079bd1755d5ca01a988d1d98ae
lgrossi <lucas.ggrossi@gmail.com>
2025-06-09 20:32:57 +0200

    Add example project using the private libs

--- test-project/main.cpp
+++ test-project/main.cpp
@@ -1,7 +1,9 @@
 #include <protocol/message.hpp>
+#include <capuera/combat.hpp>
 #include <iostream>
 
 int main() {
+    std::cout << capuera::hello() << std::endl;
     std::cout << protocol::hello() << std::endl;
     return 0;
 }
